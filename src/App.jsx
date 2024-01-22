762a51ef6b0df34f336ad6bd6167c8ba95b84afe
successijoba1 <122977363+successijoba1@users.noreply.github.com>
2024-01-22 04:20:37 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./App.css";
+import { characters } from "./MarvelData";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCharacters = characters.filter((data) =>
+    data.alias.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="mainBody">
+      <div className="topHeader">
+        <h1 className="header">Success Wrestle Mania </h1>
+        <input
+          placeholder="Search a Wrestler"
+          className="topInput"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="cardBody">
+        {filteredCharacters.map((data, index) => (
+          <div key={index} className="card">
+            <div className="main">
+              <img
+                className="avatarImage"
+                src={data.img1}
+                alt={data.alias}
+              />
+              <h2>{data.alias}</h2>
+              <p className="description">{data?.abilities[0]}</p>
+              <div className="avatarInfo">
+                <div className="price">
+                  <ins>◘</ins>
+                  <p>{data.team}</p>
+                </div>
+                <div className="duration">
+                  <ins>◷</ins>
+                  <p>Type: {data.homeworld}</p>
+                </div>
+              </div>
+              <hr />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MarvelData.js
@@ -0,0 +1,113 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "",
+    alias: "Seth Rolins",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_460/public/all/2023/06/Seth_Rollins_PROFILE--fb92833e38aeeca6dd9257428ad2b6cb.png",
+    homeworld: "Street racing",
+    abilities: [
+      "Colby Daniel Lopez (born May 28, 1986) is an American professional wrestler. He is currently signed to WWE, where he performs on the Raw brand under the ring name Seth "Freakin" Rollins. ",
+      "Powered armor suit",
+      "Superhuman strength",
+    ],
+    team: "Racing"
+  },
+  {
+    name: "",
+    alias: "Roman Reigns",
+    abilities: ["Leati Joseph Anoaʻi  born May 25, 1985) is an American professional wrestler and former American football player. He is currently signed to WWE, where he performs on the SmackDown brand under the ring name Roman Reigns. He is the current and longest-reigning WWE Universal Champion in his second reign and the current WWE Champion in his fourth reign"],
+    team: "Action",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_460/public/all/2023/06/Roman_Reigns_PROFILE--cf0a9c5af594c1d401fab44149d4a500.png",
+    homeworld: "Online CO-OP, PvP",
+    
+  },
+  {
+    name: "",
+    alias: "Damian Preist ",
+    abilities: ["Luis Martínez (born September 26, 1982) is an American professional wrestler of Puerto Rican descent. He is currently signed to WWE, where he performs on the Raw brand under the ring name Damian Priest."],
+    team: "Action",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_m__composite/public/all/2023/09/Priest_Finn-_09042023ca_116_Profile--7bb56ba9d7c4a751f1cf9e29bbebea70.png",
+    homeworld: "Online CO-OP, PvP",
+
+    description:
+      "James 'Rhodey' Rhodes, also known as War Machine, is a highly skilled pilot and a military expert. He operates a heavily armed powered armor suit that enhances his strength and combat capabilities. With a strong sense of duty and loyalty to his allies, Rhodey plays a crucial role in the Avengers' efforts to maintain peace and protect the world.",
+  },
+  {
+    name: "",
+    alias: "IO Sky",
+    abilities: ["Masami Odate ( Ōdate Masami, born May 8, 1990) is a Japanese professional wrestler. She is signed to WWE, where she performs on the SmackDown brand under the ring name Iyo Sky (イヨ・スカイ, Iyo Sukai) (stylized in all caps), where she is the current WWE Women's Champion in her first reign."],
+    team: "Action",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_460/public/all/2023/08/IO_08052023ej_1672_Ntwk_Profile--beb8e30fef36c26273c49e3f3b1a7efd.png",
+    homeworld: "Offline Simulator",
+
+    description:
+      "Riri Williams, known as Ironheart, is a prodigious young inventor with a genius-level intellect. She has designed her own advanced powered armor suit, which she uses to fight against threats as a member of the Young Avengers. Riri's intelligence, determination, and innovative spirit make her a rising star in the superhero community.",
+  },
+
+  // Spider-Man
+  {
+    name: "",
+    alias: "Rhea Ripley",
+    abilities: ["Demi Bennett (born 11 October 1996) is an Australian professional wrestler. She is currently signed to WWE, where she performs on the Raw brand under the ring name Rhea Ripley. She is a member of The Judgment Day stable and currently holds the Women's World Championship in her first reign."],
+    team: "Racing",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_460/public/all/2023/07/Rhea_RipleyProfile_title_New--01e56ce8581e7c009793a36306e813fc.png",
+    homeworld: "Street racing",
+    img3: "url_to_img3_for_Natasha_Stark",
+    description:
+      "Miles Morales, the teenage Spider-Man, has bio-electrokinesis powers, in addition to wall-crawling abilities. As a member of the Champions, Miles embodies the spirit of the Spider-Man legacy, fighting for equality and justice in the Marvel Universe while navigating the challenges of being a young hero.",
+  },
+
+  {
+    name: "AJ Styles",
+    alias: "AJ Styles",
+    abilities: ["Allen Neal Jones (born June 2, 1977), better known by his ring name AJ Styles is an American professional wrestler. He is currently signed to WWE, where he performs on the SmackDown brand, and is the leader of The O.C. stable."],
+    team: "Racing",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_m__composite/public/all/2020/10/AjOriginal_Profile--73738f78c90f05ce100f4cf7ddbea8ae.png",
+    homeworld: "Underground Racing",
+    description:
+      "Cindy Moon, codenamed Silk, is a skilled hero with the power of web-shooting and remarkable superhuman agility. She is an essential member of the Agents of Atlas, using her arachnid-like abilities to combat threats and safeguard the world.",
+  },
+
+  {
+    name: "",
+    alias: "Bayley",
+    abilities: ["Pamela Rose Martinez (born June 15, 1989) is an American professional wrestler. She is currently signed to WWE, where she performs on the SmackDown brand under the ring name Bayley, and the leader of the faction Damage CTRL. Bayley's 380-day reign as SmackDown Women's Champion stands as the longest in the championship's history."],
+    team: "Horror",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_m__composite/public/all/2022/10/Bayley_PROFILE--273098f523f8aa4826af096150e1d15e.png",
+    homeworld: "Alien, Action",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: "",
+    alias: "Charloette Flair",
+    abilities: ["Ashley Elizabeth Fliehr (born April 5, 1986) is an American professional wrestler. She is signed to WWE, where she performs on the SmackDown brand under the ring name Charlotte Flair. She is a second-generation professional wrestler, being the daughter of Ric Flair."],
+    team: "Horror",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_m__composite/public/all/2023/04/CharlotteFlair_01122023PB_0365_Profile--7190563facbfe218729ea64a62b877f8.png",
+    homeworld: "Alien, Action",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },  {
+    name: "",
+    alias: "Fin Balor",
+    abilities: ["Fergal Devitt (born 25 July 1981) is an Irish professional wrestler. He is currently signed to WWE, where he performs on the Raw brand under the ring name Finn Bálor (/ˈbælər/). He is a member of The Judgment Day stable and is one-half of the Undisputed WWE Tag Team Champions with stablemate Damian Priest in their second reign. "],
+    team: "Horror",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_m__composite/public/all/2023/09/Priest_Finn-_09042023ca_498_profile--532f3ecdb3ceed7d198ecba1e61d2fc4.png",
+    homeworld: "Alien, Action",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  },
+  {
+    name: "",
+    alias: "Jimmy Uso",
+    abilities: ["Jonathan Solofa Fatu (born August 22, 1985) is an American professional wrestler. He is currently signed to WWE, where he performs on the SmackDown brand under the ring name Jimmy Uso, and is a member of The Bloodline. He spent most of his career with his twin brother Jey Uso as The Usos. He is a member of wrestling's renowned Anoa'i family. His bloodline includes his father Rikishi and his great uncles the Wild Samoans."],
+    team: "Horror",
+    img1: "https://www.wwe.com/f/styles/wwe_1_1_m__composite/public/all/2018/03/Jimmy_Uso_Pro--d2824230adf829eb15c347840df00c92.png",
+    homeworld: "Alien, Action",
+    description:
+      "Steve Rogers, Captain America, is the embodiment of honor and virtue. Enhanced with peak human strength and wielding a Vibranium shield, he stands as a symbol of justice and resilience. Steve's unwavering commitment to the ideals of heroism makes him a cornerstone of the Avengers and the Marvel Universe.",
+  }
+  
+];
+
+console.log(characters);
